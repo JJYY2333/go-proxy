58682671199a97c60d15a7cdb8d63fbcb3672d78
JJYY2333 <824778124@qq.com>
2022-03-19 00:42:58 +0800

    # 增加shadow的开发思路记录

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ Step 1.
 
 Step 2.
     shadow.
+
+How to shadow?
+    shadow的功能就是将一个普通的Conn转化为具有加密/解密能力的Conn。
+    而基本上所有的加密/解密算法的操作对象都是一个固定大小的数据块（末尾数据块会进行填充操作）
+    所以，可以通过以下的方法实现一个具有加密解密功能的Conn: 将Conn获取的数据先放入缓存池进行解密再提交上层，将Conn要传送的数据先放入缓存池加密再进行传输
+        为了方便复用与管理，我们将对读取数据和写入数据改为Writer与Reader
+        所以Conn具有的模块应该包括：
+            1. net.Conn, 负载数据传输
+            2. Cipher, 加密算法
+            3. Nonce(key),
+            4. Writer,
+            5. Reader,
+        对于Writer, 我们在io.Writer上又对其做了一层封装, 它的属性有
+            1. io.Writer
+            2. Cipher,
+            3. Nonce
+            4. buf []
+        对于Reader，也是一样的思路 
+            1. io.Writer
+            2. Cipher,
+            3. Nonce 
+            4. buf []
+            
+    所以下一步要做的，就是研究Cipher的实现与使用，并且对Writer和Reader进行封装
---
+++ testCipher/main.go
@@ -0,0 +1,82 @@
+//package main
+//
+//import (
+//	cryptorand "crypto/rand"
+//	"fmt"
+//	"golang.org/x/crypto/chacha20poly1305"
+//	"log"
+//)
+//
+//func Cha(key []byte) {
+//	aead, err := chacha20poly1305.NewX(key)
+//	if err != nil {
+//		log.Fatalln("Failed to instantiate XChaCha20-Poly1305:", err)
+//	}
+//
+//	for _, msg := range []string{
+//		"Attack at dawn.",
+//		"The eagle has landed.",
+//		"Gophers, gophers, gophers everywhere!",
+//	} {
+//		// Encryption.
+//		nonce := make([]byte, chacha20poly1305.NonceSizeX)
+//		if _, err := cryptorand.Read(nonce); err != nil {
+//			panic(err)
+//		}
+//		ciphertext := aead.Seal(nil, nonce, []byte(msg), nil)
+//
+//		// Decryption.
+//		plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
+//		if err != nil {
+//			log.Fatalln("Failed to decrypt or authenticate message:", err)
+//		}
+//
+//		fmt.Printf("%s\n", plaintext)
+//	}
+//}
+//
+//func main() {
+//	var key = make([]byte, chacha20poly1305.KeySize)
+//	fmt.Printf("", key)
+//	Cha(key)
+//}
+
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	// Load your secret key from a safe place and reuse it across multiple
+	// Seal/Open calls. (Obviously don't use this example key for anything
+	// real.) If you want to convert a passphrase to a key, use a suitable
+	// package like bcrypt or scrypt.
+	// When decoded the key should be 16 bytes (AES-128) or 32 (AES-256).
+	key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
+	ciphertext, _ := hex.DecodeString("c3aaa29f002ca75870806e44086700f62ce4d43e902b3888e23ceff797a7a471")
+	nonce, _ := hex.DecodeString("64a9433eae7ccceee2fc0eda")
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	println("block: ", block.BlockSize())
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	println("aesgcm1: ", aesgcm.NonceSize())
+	println("aesgcm2: ", aesgcm.Overhead())
+
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("%s\n", plaintext)
+}
