b5ec1b6e249b73ed1bc7d8bbe2885ac2be8508e2
JJYY2333 <824778124@qq.com>
2022-03-20 00:47:28 +0800

    # 完成cipher和生成模块

--- common/shadow/cipher.go
+++ common/shadow/cipher.go
@@ -6,10 +6,11 @@ import (
 	"encoding/base64"
 	"io"
 	"net"
+	"encoding/hex"
 	//"errors"
-	//
+	"strconv"
 	//"crypto"
-	//"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/chacha20poly1305"
 	// "golang.org/x/crypto/hkdf"
 )
 
@@ -18,14 +19,6 @@ import (
 	默认由外部调用提供keySize和key参数
 */
 
-// 生成密钥
-func KEYGen(keySize int) ([]byte, string) {
-	randKey := make([]byte, keySize)
-	io.ReadFull(rand.Reader, randKey)
-	key := base64.URLEncoding.EncodeToString(randKey)
-	// 返回生成的key数组和字符串key
-	return randKey, key
-}
 
 // ---------------dummy cipher------------------
 type dummy struct{}
@@ -38,6 +31,43 @@ func (dummy) PacketConn(con net.Conn) net.Conn {
 	return con
 }
 
+
+// 生成密钥
+func KEYGen(keySize int) ([]byte, string) {
+	randKey := make([]byte, keySize)
+	io.ReadFull(rand.Reader, randKey)
+	key := base64.URLEncoding.EncodeToString(randKey)
+	// 返回生成的key数组和字符串key
+	return randKey, key
+}
+const(
+	StringKeyOf16 = "6368616e676520746869732070617373"
+	StringKeyOf32 = "6368616e676520746869732070617373776f726420746f206120736563726574"
+
+	// AEAD cipher, 可选加密算法
+	aeadAes128Gcm        = "AEAD_AES_128_GCM"
+	aeadAes256Gcm        = "AEAD_AES_256_GCM"
+	aeadChacha20Poly1305 = "AEAD_CHACHA20_POLY1305"
+)
+
+func GetKeyByDecode(stringKey string) []byte{
+	key, _:= hex.DecodeString(stringKey)
+	return key
+}
+
+// hardcode的key
+var Key = map[int]struct{
+	keySize int
+	keyString string
+	getKey func(string) []byte
+}{
+	16: {16, StringKeyOf16, GetKeyByDecode},
+	32: {32, StringKeyOf32, GetKeyByDecode},
+}
+
+// 用的时候，a.getKey(a.keyString)
+
+
 // -------------AEAD cipher-----------------
 
 /*
@@ -49,20 +79,6 @@ fmt.Println(base64.URLEncoding.EncodeToString(key))
 key, _ := hex.DecodeString(randKey)
 */
 
-// AEAD cipher, 可选加密算法
-const (
-	aeadAes128Gcm        = "AEAD_AES_128_GCM"
-	aeadAes256Gcm        = "AEAD_AES_256_GCM"
-	aeadChacha20Poly1305 = "AEAD_CHACHA20_POLY1305"
-)
-
-// 定义一个加密算子应该有的基本功能
-type Cipher interface {
-	KeySize() int
-	SaltSize() int
-	Encrypter(salt []byte) (cipher.AEAD, error)
-	Decrypter(salt []byte) (cipher.AEAD, error)
-}
 
 /*
 KeySizeError的写法:
@@ -74,6 +90,89 @@ func (e KeySizeError) Error() string {
 }
 */
 
+// List of AEAD ciphers: key size in bytes and constructor
+var aeadList = map[string]struct {
+	KeySize int
+	New     func([]byte) (shadowaead.Cipher, error)
+}{
+	aeadAes128Gcm:        {16, AESGCM},
+	aeadAes256Gcm:        {32, AESGCM},
+	aeadChacha20Poly1305: {32, Chacha20Poly1305},
+}
+
+// 定义一个加密算子应该有的基本功能
+type Cipher interface {
+	KeySize() int
+	// 第一期中不会使用salt对key进行二次hash，留一个接口
+	SaltSize() int
+	// 如果没有用salt升级key就用key，否则用salt
+	Encrypter(key []byte) (cipher.AEAD, error)
+	Decrypter(key []byte) (cipher.AEAD, error)
+}
+
+// KeySizeError
+type KeySizeError int
+
+func (e KeySizeError) Error() string {
+	return "key size error: need " + strconv.Itoa(int(e)) + " bytes"
+}
+
+type metaCipher struct {
+	// psk就是key
+	psk      []byte
+	makeAEAD func(key []byte) (cipher.AEAD, error)
+}
+
+func (a *metaCipher) KeySize() int { return len(a.psk) }
+func (a *metaCipher) SaltSize() int {
+	if ks := a.KeySize(); ks > 16 {
+		return ks
+	}
+	return 16
+}
+func (a *metaCipher) Encrypter(key []byte) (cipher.AEAD, error) {
+	//salt=key, 下面的操作都是根据salt和subkey对key进行二次hash，生成新的key
+	//subkey := make([]byte, a.KeySize())
+	//hkdfSHA1(a.psk, salt, []byte("ss-subkey"), subkey)
+	return a.makeAEAD(key)
+}
+func (a *metaCipher) Decrypter(key []byte) (cipher.AEAD, error) {
+	//subkey := make([]byte, a.KeySize())
+	//hkdfSHA1(a.psk, salt, []byte("ss-subkey"), subkey)
+	return a.makeAEAD(key)
+}
+
+// AESGCM creates a new Cipher with a pre-shared key. len(psk) must be
+// one of 16, 24, or 32 to select AES-128/196/256-GCM.
+func AESGCM(key []byte) (Cipher, error) {
+	switch l := len(key); l {
+	case 16, 24, 32: // AES 128/196/256
+	default:
+		return nil, aes.KeySizeError(l)
+	}
+
+	// 生成新的blk，
+	blk, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &metaCipher{psk: key, makeAEAD: cipher.NewGCM(blk)}, nil
+}
+
+
+// Chacha20Poly1305 creates a new Cipher with a pre-shared key. len(psk)
+// must be 32.
+func Chacha20Poly1305(psk []byte) (Cipher, error) {
+	if len(psk) != chacha20poly1305.KeySize {
+		return nil, KeySizeError(chacha20poly1305.KeySize)
+	}
+	return &metaCipher{psk: psk, makeAEAD: chacha20poly1305.New}, nil
+}
+
+
+
+/*
 type Cipher interface {
 	PacketConnCipher
 	StreamConnCipher
@@ -86,3 +185,4 @@ type PacketConnCipher interface {
 type StreamConnCipher interface {
 	StreamConn(net.Conn) net.Conn
 }
+*/
