6be6940c4d984eaa7eda616ebbab50c81bf46448
neil <958862317@qq.com>
2022-03-20 12:02:54 +0800

    # 把certs移动到根目录下， 把certs脚本放到cmd下

---
+++ common/auth/auth.go
@@ -0,0 +1,7 @@
+/*
+@Time    : 3/20/22 10:19
+@Author  : Neil
+@File    : auth.go
+*/
+
+package auth
---
+++ network/connection_util.go
@@ -0,0 +1,7 @@
+/*
+@Time    : 3/20/22 11:34
+@Author  : Neil
+@File    : connection_util
+*/
+
+package network
