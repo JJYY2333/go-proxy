042b28f28e1c24b3f5f26916b513a027de3045c9
neil <958862317@qq.com>
2022-03-20 12:06:55 +0800

    # 配置处理

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ package main
 
 import (
 	"flag"
+	"go-proxy/v1/common/auth"
 	"go-proxy/v1/common/config"
 	"go-proxy/v1/network/tcp"
 	"go-proxy/v1/network/tls"
+	"go-proxy/v1/socks"
 	"log"
 	"net"
 	"os"
@@ -31,24 +33,26 @@ func main() {
 	cfg := config.New()
 	cfg.LoadConfigFromFile(*path)
 
+	socks := socks.NewSocks(cfg.UseAuth, auth.NewDummyAuth())
+
 	if cfg.Connection == "tcp" {
 		if cfg.Mode == "local" {
-			go tcp.TcpLocal(cfg.LocalAddr, cfg.RemoteAddr, dummy)
+			go tcp.TcpLocal(cfg.LocalAddr, cfg.RemoteAddr, dummy, socks)
 		}else if cfg.Mode == "remote" {
 			go tcp.TcpRemote(cfg.ListenAddr, dummy)
 		}else if cfg.Mode == "test" {
-			go tcp.TcpLocal(cfg.LocalAddr, cfg.RemoteAddr, dummy)
+			go tcp.TcpLocal(cfg.LocalAddr, cfg.RemoteAddr, dummy, socks)
 			go tcp.TcpRemote(cfg.ListenAddr, dummy)
 		} else {
 			log.Fatalf("invalid mode in config: %v", cfg.Mode)
 		}
 	} else if cfg.Connection == "tls" {
 		if cfg.Mode == "local" {
-			go tls.TLSLocal(cfg.LocalAddr, cfg.RemoteAddr)
+			go tls.TLSLocal(cfg.LocalAddr, cfg.RemoteAddr, socks)
 		}else if cfg.Mode == "remote" {
 			go tls.TLSRemote(cfg.ListenAddr)
 		}else if cfg.Mode == "test" {
-			go tls.TLSLocal(cfg.LocalAddr, cfg.RemoteAddr)
+			go tls.TLSLocal(cfg.LocalAddr, cfg.RemoteAddr, socks)
 			go tls.TLSRemote(cfg.ListenAddr)
 		} else {
 			log.Fatalf("invalid mode in config: %v", cfg.Mode)
