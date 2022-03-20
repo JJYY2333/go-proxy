8087cebb2bc9dc15c6fb53bda6a84545e42f8ba3
neil <958862317@qq.com>
2022-03-20 09:58:30 +0800

    # modified config.go to support tls

--- main.go
+++ main.go
@@ -7,21 +7,53 @@
 package main
 
 import (
+	"flag"
 	"go-proxy/v1/common/config"
 	"go-proxy/v1/network/tcp"
+	"go-proxy/v1/network/tls"
+	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
+var path = flag.String("path", "conf/app_test.ini", "set the ini file path")
+
 func main() {
+
+	flag.Parse()
+
 	dummy := func(conn net.Conn) net.Conn {
 		return conn
 	}
 
-	go tcp.TcpLocal(config.LocalAddr, config.RemoteAddr, dummy)
-	go tcp.TcpRemote(config.RemoteAddr, dummy)
+	cfg := config.New()
+	cfg.LoadConfigFromFile(*path)
+
+	if cfg.Connection == "tcp" {
+		if cfg.Mode == "local" {
+			go tcp.TcpLocal(cfg.LocalAddr, cfg.RemoteAddr, dummy)
+		}else if cfg.Mode == "remote" {
+			go tcp.TcpRemote(cfg.ListenAddr, dummy)
+		}else if cfg.Mode == "test" {
+			go tcp.TcpLocal(cfg.LocalAddr, cfg.RemoteAddr, dummy)
+			go tcp.TcpRemote(cfg.ListenAddr, dummy)
+		} else {
+			log.Fatalf("invalid mode in config: %v", cfg.Mode)
+		}
+	} else if cfg.Connection == "tls" {
+		if cfg.Mode == "local" {
+			go tls.TLSLocal(cfg.LocalAddr, cfg.RemoteAddr)
+		}else if cfg.Mode == "remote" {
+			go tls.TLSRemote(cfg.ListenAddr)
+		}else if cfg.Mode == "test" {
+			go tls.TLSLocal(cfg.LocalAddr, cfg.RemoteAddr)
+			go tls.TLSRemote(cfg.ListenAddr)
+		} else {
+			log.Fatalf("invalid mode in config: %v", cfg.Mode)
+		}
+	}
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
