26783c5a1d7538e4b292507388d729eb9d04951d
neil <958862317@qq.com>
2022-03-25 22:28:17 +0800

    #14 为了设置tcp shadow, shadow package 增加了部分接口， 但是因为shadow pkg报错，故先用tmp代替

--- common/shadow/shadow.go
+++ common/shadow/shadow.go
@@ -1,2 +1,28 @@
 package shadow
 
+import (
+	"fmt"
+	"net"
+)
+
+var (
+	shadowFuncMap map[string]func(net.Conn) net.Conn
+)
+
+func init() {
+	shadowFuncMap = make(map[string]func(net.Conn) net.Conn)
+	shadowFuncMap["dummy"] = myDummy
+}
+
+func GetShadow(name string) (func(net.Conn) net.Conn, error) {
+	f, ok := shadowFuncMap[name]
+	if !ok {
+		return nil, fmt.Errorf("failed to find shadow type: %s", name)
+	}
+
+	return f, nil
+}
+
+func myDummy(conn net.Conn) net.Conn {
+	return conn
+}
--- common/tmpShadow/shadow.go
+++ common/tmpShadow/shadow.go
@@ -5,3 +5,30 @@
 */
 
 package tmpShadow
+
+import (
+	"fmt"
+	"net"
+)
+
+var (
+	shadowFuncMap map[string]func(net.Conn) net.Conn
+)
+
+func init() {
+	shadowFuncMap = make(map[string]func(net.Conn) net.Conn)
+	shadowFuncMap["dummy"] = myDummy
+}
+
+func GetShadow(name string) (func(net.Conn) net.Conn, error) {
+	f, ok := shadowFuncMap[name]
+	if !ok {
+		return nil, fmt.Errorf("failed to find shadow type: %s", name)
+	}
+
+	return f, nil
+}
+
+func myDummy(conn net.Conn) net.Conn {
+	return conn
+}
