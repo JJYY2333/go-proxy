78b58a899a3f14a7ff4390beb3b5aff3fc5dc40c
neil <958862317@qq.com>
2022-03-26 11:16:43 +0800

    #9 stat pkg是用来做流量统计的

--- common/statistics/stat.go
+++ common/statistics/stat.go
@@ -5,3 +5,31 @@
 */
 
 package statistics
+
+import (
+	"log"
+	"sync"
+)
+
+var BytesAccount *Stat
+
+func init() {
+	BytesAccount = &Stat{cache: make(map[string]int64)}
+}
+
+type Stat struct {
+	mu    sync.Mutex
+	cache map[string]int64
+}
+
+func (stat *Stat) Add(key string, delta int64) {
+	stat.mu.Lock()
+	defer stat.mu.Unlock()
+	stat.cache[key] += delta
+	log.Printf("cache: %v", stat.cache)
+	return
+}
+
+func (stat *Stat) save() {
+
+}
